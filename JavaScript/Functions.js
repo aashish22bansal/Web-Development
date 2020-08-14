30384bd6ab35a6b839ff5bff51e0d692b9bd134d
VITGEARSHCS <aashish.bansal2019@vitstudent.ac.in>
2020-08-14 16:54:56 +0530

    Faculty's Material

---
+++ JavaScript/Functions.js
@@ -0,0 +1,5 @@
+function product(a,b)
+{
+    return a*b;
+}
+document.write(product(4,3));
---
+++ JavaScript/Getting_User_Input.js
@@ -0,0 +1,2 @@
+var name = window.prompt("Enter your name:");
+document.write("Hey "+name+"! How's your day going?");
