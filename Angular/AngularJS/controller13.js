f9567ecc54563639c00f1685a664507e8f2afc57
Aashish Bansal <aashish22bansal@gmail.com>
2020-10-02 00:17:59 +0530

    AngularJS

--- Angular/AngularJS/controller13.js
+++ Angular/AngularJS/controller13.js
@@ -3,7 +3,9 @@ var app=angular.module('mainApp',['ngRoute']);
 app.config(function($routeProvider){
     $routeProvider
     .when('/',{
+        template: '<b>This text is bold</b>',
         templateUrl: 'page.html'
+        /*In this, 'template' has higher preferrence as compared to 'templateUrl'. If template is specified, then templateUrl will not work.*/
     })
     .when('/helloUser',{
         templateUrl: 'hello.html'
