4c58d7bd35216dd5025547bc4aded8c6f346d35a
Aashish Bansal <aashish22bansal@gmail.com>
2021-01-13 23:19:08 +0530

    Errors emerging

--- MeteorJs/resolutions/client/main.js
+++ MeteorJs/resolutions/client/main.js
@@ -1,5 +1,7 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
+import { Mongo } from 'meteor/mongo';
+import { Tasks } from '..imports//api/tasks.js';
 
 import './main.html';
 /*
@@ -27,6 +29,19 @@ if(Meteor.isClient){
     Template.body.helpers({
         resolutions: function(){
             return Resolutions.find();
+        }
+    });
+    Template.body.events({
+        'submit .new-resolution': function(event){
+            var title = event.target.title.value;
+
+            Resolutions.insert({
+                title: title,
+                createdAt: new Date()
+            });
+            event.target.title.value = "";
+
+            return false;
         }
     });
 }
